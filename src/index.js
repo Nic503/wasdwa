2a12dc48cdf8e116e2013ce3e43f78cd16503938
Nic503 <81273720+Nic503@users.noreply.github.com>
2026-04-22 18:02:37 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
+import './style.css'
+import CARTS from './views/carts'
+import Inventory from './views/inventory'
+import GEEKS from './views/geeks'
+import FLOWER from './views/flower'
+import NotFound from './views/not-found'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={CARTS} exact path="/carts" />
+        <Route component={Inventory} exact path="/inventory" />
+        <Route component={GEEKS} exact path="/" />
+        <Route component={FLOWER} exact path="/flower" />
+        <Route component={NotFound} path="**" />
+        <Redirect to="**" />
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
